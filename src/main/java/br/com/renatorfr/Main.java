8b858595321c50b6434ce025a287ee3bb98d207e
Renato Freire Ricardo <renatorfr@gmail.com>
2016-02-09 17:35:37 -0200

    Changing from gradle to maven

--- src/main/java/br/com/renatorfr/Main.java
+++ src/main/java/br/com/renatorfr/Main.java
@@ -1,11 +1,25 @@
 package br.com.renatorfr;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 import static br.com.renatorfr.controller.ControllersEnum.TRAINING_CONTROLLER;
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        get("/", (request, response) -> "Hello Spark!");
-        get("/training", (request, response) -> RequestHandler.handle(TRAINING_CONTROLLER, request));
+        get("/", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return "Hello Spark!";
+            }
+        });
+        get("/training", new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return RequestHandler.handle(TRAINING_CONTROLLER, request);
+            }
+        });
     }
 }
