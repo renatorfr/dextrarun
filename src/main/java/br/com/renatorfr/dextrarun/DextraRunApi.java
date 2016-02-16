577d52023d21236a322e2912b5332bae087f50fb
Renato Freire Ricardo <renatorfr@gmail.com>
2016-02-15 23:40:50 -0200

    Adding authentication

--- src/main/java/br/com/renatorfr/dextrarun/DextraRunApi.java
+++ src/main/java/br/com/renatorfr/dextrarun/DextraRunApi.java
@@ -8,25 +8,38 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import com.google.api.server.spi.config.Named;
+import com.google.appengine.api.oauth.OAuthRequestException;
+import com.google.appengine.api.users.User;
 
 @Api(name = "dextraRunApi",
      version = "v1",
+     clientIds = {Constants.WEB_CLIENT_ID},
      namespace = @ApiNamespace(ownerDomain = "dextrarun.renatorfr.com.br",
                                ownerName = "renatorfr",
                                packagePath = ""))
 public class DextraRunApi {
     @ApiMethod()
-    public TrainingResponse getTraining(@Named("trainingId") Long id) {
+    public TrainingResponse getTraining(@Named("trainingId") Long id, User user) throws OAuthRequestException {
+        validate(user);
+
         Training training = new TrainingRepositoryStub().getTraining(id);
 
         return new TrainingResponse(training);
     }
 
     @ApiMethod(httpMethod = ApiMethod.HttpMethod.POST)
-    public TrainingResponse addTraining(@Named("name") String name, @Named("jediMasterId") Long jediMaster, @Named("padwanId") Long padwan) {
+    public TrainingResponse addTraining(@Named("name") String name, @Named("jediMasterId") Long jediMaster, @Named("padwanId") Long padwan, User user) throws OAuthRequestException {
+        validate(user);
+
         TrainingRequest trainingRequest = new TrainingRequest(0L, jediMaster, padwan, name);
         Training training = trainingRequest.createTraining();
 
         return new TrainingResponse(training);
+    }
+
+    private void validate(User user) throws OAuthRequestException {
+        if (user == null) {
+            throw new OAuthRequestException("Forbidden");
+        }
     }
 }
