bad1225839d2bdc2325c94450124c193a8eccf92
Renato Freire Ricardo <renatorfr@gmail.com>
2016-04-04 21:53:49 -0300

    Removing unused endpoints

--- web/src/main/java/br/com/renatorfr/dextrarun/DextraRunApi.java
+++ web/src/main/java/br/com/renatorfr/dextrarun/DextraRunApi.java
@@ -13,31 +13,17 @@ import com.google.appengine.api.oauth.OAuthRequestException;
 import com.google.appengine.api.users.User;
 
 @Api(name = "dextraRunApi",
-        version = "v1",
-        clientIds = { Constants.WEB_CLIENT_ID, Constant.API_EXPLORER_CLIENT_ID },
-        namespace = @ApiNamespace(ownerDomain = "dextrarun.renatorfr.com.br",
-                ownerName = "renatorfr",
-                packagePath = ""))
+     version = "v1",
+     clientIds = {Constants.WEB_CLIENT_ID, Constant.API_EXPLORER_CLIENT_ID},
+     namespace = @ApiNamespace(ownerDomain = "dextrarun.renatorfr.com.br",
+                               ownerName = "renatorfr",
+                               packagePath = ""))
 public class DextraRunApi {
     @ApiMethod()
     public TrainingVM getTraining(@Named("trainingId") Long id, User user) throws OAuthRequestException {
         validate(user);
 
         Training training = new TrainingRepositoryObjectify().get(id);
-
-        return new TrainingVM(training);
-    }
-
-    @ApiMethod(httpMethod = ApiMethod.HttpMethod.POST)
-    public TrainingVM addTraining(@Named("name") String name, @Named("jediMasterId") Long jediMaster, @Named("padwanId") Long padwan, User user)
-            throws OAuthRequestException {
-        validate(user);
-
-        TrainingVM trainingVM = new TrainingVM(null, jediMaster, padwan, name, null);
-        Training training = trainingVM.createTraining();
-
-        TrainingRepository trainingRepository = new TrainingRepositoryObjectify();
-        trainingRepository.save(training);
 
         return new TrainingVM(training);
     }
