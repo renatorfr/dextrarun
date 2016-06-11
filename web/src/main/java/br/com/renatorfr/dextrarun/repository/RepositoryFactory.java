539d0119f5d9f2fbc1ebd3c997ac436f87ee73c0
Renato Freire Ricardo <renatorfr@gmail.com>
2016-06-10 22:02:04 -0300

    Init Firebase project

--- web/src/main/java/br/com/renatorfr/dextrarun/repository/RepositoryFactory.java
+++
@@ -1,5 +0,0 @@
-package br.com.renatorfr.dextrarun.repository;
-
-public class RepositoryFactory {
-
-}
--- web/start_chrome.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/google-chrome-stable --user-data-dir=test --unsafely-treat-insecure-origin-as-secure=http://localhost:8080
